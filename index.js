1d1cbb0fd0413374a1c2d33f4f865fca4370d78f
sagarjhathi <sagarjhathii@gmail.com>
2024-11-15 22:33:17 +0530

    Adding new set of rules for intri file

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ console.log("helloworld into file latestncj")
 
 console.log("helloworld")
 
+
+//Adding new set of rules
+console.log("helloworld   01")
+console.log("helloworld    02")
+console.log("helloworld    03")
+//Added another rule
+console.log("helloworld    04")
+
+
